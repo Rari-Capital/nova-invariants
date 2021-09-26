9fc2ddb72368db711bf13a8d5a41f3f0f8eb8254
t11s <greenfeatherhelp@gmail.com>
2021-09-25 17:34:55 -0700

    Simplify

--- src/ExecutionManager.t.sol
+++ src/ExecutionManager.t.sol
@@ -18,49 +18,8 @@ contract ExecutionManagerTest is DSTestPlus {
     }
 
     /*///////////////////////////////////////////////////////////////
-                              INVARIANTS
-    //////////////////////////////////////////////////////////////*/
-
-    function invariantGasConfig() public {
-        (
-            uint32 calldataByteGasEstimate,
-            uint96 missingGasEstimate,
-            uint96 strategyCallGasBuffer,
-            uint32 execCompletedMessageGasLimit
-        ) = executionManager.gasConfig();
-
-        assertUint32Eq(calldataByteGasEstimate, 13);
-        assertUint96Eq(missingGasEstimate, 200000);
-        assertUint96Eq(strategyCallGasBuffer, 5000);
-        assertUint32Eq(execCompletedMessageGasLimit, 1500000);
-    }
-
-    function invariantCurrentExecHash() public {
-        assertEq(executionManager.currentExecHash(), executionManager.DEFAULT_EXECHASH());
-    }
-
-    /*///////////////////////////////////////////////////////////////
                         PROPERTY/SYMBOLIC TESTS
     //////////////////////////////////////////////////////////////*/
 
     function proveUpdatingGasConfig(L1_NovaExecutionManager.GasConfig calldata newGasConfig) public {}
-
-    function proveFailTransferFromRelayerOutsideOfActiveExec(address token, uint256 amount) public {
-        executionManager.transferFromRelayer(token, amount);
-    }
-
-    function testExec(
-        uint256 nonce,
-        address strategy,
-        bytes calldata l1Calldata,
-        uint256 gasLimit,
-        address recipient,
-        uint256 deadline
-    ) public {
-        try executionManager.exec(nonce, strategy, l1Calldata, gasLimit, recipient, deadline) {
-            assert(executionManager.currentExecHash() == executionManager.DEFAULT_EXECHASH());
-            assert(executionManager.currentRelayer() == address(this));
-            assert(executionManager.currentlyExecutingStrategy() == strategy);
-        } catch {}
-    }
 }
